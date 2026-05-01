23f824fb47945f8098de3c96c5129bb622a8c4e0
Apostolos Chalis <tolishalis@gmail.com>
2026-05-01 21:07:55 +0300

    Wrote Downloader class

--- include/Downloader.hpp
+++ include/Downloader.hpp
@@ -1,1 +1,30 @@
 // Author: Apostolos Chalis 2026 <achalis@csd.auth.gr>
+#ifndef DOWNLOADER_HPP
+#define DOWNLOADER_HPP
+
+#include <QObject>
+#include <QString>
+#include <QDebug>
+
+class Downloader : public QObject {
+	Q_OBJECT
+
+	private:
+		QString name_of_university;
+		QString name_of_department;
+
+	public:
+		explicit Downloader(const QString &university, const QString &department, QObject *parent = nullptr)
+			: QObject(parent), name_of_university(university), name_of_department(department) {}
+
+		QString check_package_manager();
+		void download_pacman();
+		void download_apt();
+
+	signals:
+		void progress_updated(int percentage);
+		void status_message(const QString &message);
+		void download_completed(bool success);
+};
+
+#endif // DOWNLOADER_HPP
---
+++ src/Downloader.cpp
@@ -0,0 +1,6 @@
+// Author: Apostolos Chalis 2026 <achalis@csd.auth.gr>
+#include "Downloader.hpp"
+
+#include <QDebug> 
+
+
