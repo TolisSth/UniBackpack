210f4b4486d19d5749d424df3280409b7ba66565
Ioannis Michadasis <johnmichadasis@gmail.com>
2026-05-02 22:02:39 +0300

    Add is_in_apt_repo & wrote if condition for apt

--- src/Downloader.cpp
+++ src/Downloader.cpp
@@ -1,4 +1,4 @@
-// Author: Apostolos Chalis 2026 <achalis@csd.auth.gr>
+// Authors: Apostolos Chalis 2026 <achalis@csd.auth.gr>, Ioannis Michadasis 2026 <cs05415@uowm.gr>
 #include "Downloader.hpp"
 
 #include <QDebug> 
@@ -33,6 +33,21 @@ bool Downloader::is_in_pacman_repo(const QString &package_name) {
         return true;
     } else {
 	    qDebug() << "Package" << package_name << "is NOT available in pacman repositories.";
+        return false;
+    }
+}
+
+bool Downloader::is_in_apt_repo(const QString &package_name) {
+    QProcess process;
+    
+    process.start("apt-cache", QStringList() << "show" << package_name);
+    process.waitForFinished();
+
+    if (process.exitCode() == 0) {
+        qDebug() << "Package" << package_name << "is available in apt repositories.";
+        return true;
+    } else {
+        qDebug() << "Package" << package_name << "is NOT available in apt repositories.";
         return false;
     }
 }
@@ -94,7 +109,60 @@ QStringList Downloader::read_package_list(bool standard_package_manager, QString
 		return installable_with_non_standard_package_manager; 
 	}
 
-	// TODO: Write the same about apt 
+	if (standard_package_manager && package_manager == "apt") {
+		QString filepath_of_list = ":/lists/" + name_of_university + "/" + name_of_department + "/apt_list.txt";
+
+		qDebug() << "Reading package list from: " << filepath_of_list;
+		
+		QFile file(filepath_of_list);
+
+		if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            		qDebug() << "Critical Error: Could not open the file!" << file.errorString();
+			return installable_with_standard_package_manager;
+        	}
+
+		QTextStream in(&file);
+
+    		while (!in.atEnd()) {
+        		QString package = in.readLine().trimmed();
+        
+        		if (!package.isEmpty() && is_in_apt_repo(package)) {
+				qDebug() << "Adding package to installable list: " << package;
+            			installable_with_standard_package_manager.append(package);
+        		}
+    		}
+		file.close(); 
+		return installable_with_standard_package_manager;
+	}
+
+	if (!standard_package_manager && package_manager == "apt") {
+		QString filepath_of_list = ":/lists/" + name_of_university + "/" + name_of_department + "/apt_list.txt";
+
+		qDebug() << "Reading package list from: " << filepath_of_list;
+		
+		QFile file(filepath_of_list);
+
+		if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            		qDebug() << "Critical Error: Could not open the file!" << file.errorString();
+			return installable_with_non_standard_package_manager;
+        	}
+
+		QTextStream in(&file);
+
+    		while (!in.atEnd()) {
+        		QString package = in.readLine().trimmed();
+        
+        		if (!package.isEmpty() && is_in_apt_repo(package)) {
+				qDebug() << "Adding package to installable list: " << package;
+            			installable_with_non_standard_package_manager.append(package);
+        		}
+    		}
+		file.close(); 
+		return installable_with_non_standard_package_manager; 
+	}
+
+	qDebug() << "Critical Error: Unsupported package manager:" << package_manager;
+	return {};
 }
 
 void Downloader::download_via_pacman(const QStringList &list_to_be_downloaded) {
