486e3577f495a2004766245e56dec520907fd959
Apostolos Chalis <tolishalis@gmail.com>
2026-05-02 17:00:14 +0300

    Finished check_package_manager()

--- src/Downloader.cpp
+++ src/Downloader.cpp
@@ -2,5 +2,19 @@
 #include "Downloader.hpp"
 
 #include <QDebug> 
+#include <QString> 
+#include <QStringList>
+#include <QStandardPaths>
 
-
+QString Downloader::check_package_manager() {
+	if (!QStandardPaths::findExecutable("pacman").isEmpty()){
+		qDebug() << "Pacman found.";
+		return "pacman";
+	} else if (!QStandardPaths::findExecutable("apt").isEmpty()) {
+		qDebug() << "Apt found.";
+		return "apt";
+	} else {
+		qDebug() << "No supported package manager found.";
+		return "Unsupported";
+	}
+}
